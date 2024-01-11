f6a9145bf6409b2486bf60a0808a0ed36494daeb
Francesco <napo.fra96@gmail.com>
2024-01-11 16:58:35 +0100

    comments click & load

--- src/__tests__/SingleComment.test.jsx
+++ src/__tests__/SingleComment.test.jsx
@@ -1,11 +1,20 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import App from "../App";
 
-describe("check single book", () => {
-  it("check selected", () => {
+describe("check single comment", () => {
+  it("check comment not in DOM", () => {
     render(<App />);
 
     const singleCommentElement = screen.queryAllByTestId("single-comment");
     expect(singleCommentElement).toHaveLength(0);
+  });
+
+  it("check comments click & load", async () => {
+    render(<App />);
+    const allTheBooks = screen.getAllByTestId("card-selected");
+    const firstBook = allTheBooks[0];
+    fireEvent.click(firstBook);
+    const singleCommentElement = await screen.findAllByTestId("single-comment");
+    expect(singleCommentElement).not.toHaveLength(0);
   });
 });
